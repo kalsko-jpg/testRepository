0e67fdc54c8ba17512622eee66c8eebe91d2b1f2
Kalle Skogh <kalsko041104@student.jenseneducation.se>
2026-06-01 15:33:36 +0200

    This commit adds a small javascript program which logs one of three answers depending on the value of Doe

---
+++ simpleSwitch.js
@@ -0,0 +1,15 @@
+//This is a simple program to test commiting a javascript file.
+
+const Doe = 'John'
+
+switch(Doe) {
+  case 'John':
+        console.log('Welcome John Doe!');
+        break;
+  case 'Jane':
+        console.log('Welcome Jane Doe!');
+        break;
+  default:
+        console.log('Huh... Something went wrong!');
+        break;
+}
